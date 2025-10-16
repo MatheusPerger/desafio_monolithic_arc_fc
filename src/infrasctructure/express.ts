aa806a8e41983add49dba3cfa459a62414c3c1b8
MatheusPerger <matheusperger@gmail.com>
2025-10-16 08:24:57 -0300

    add ClientRouter in express

--- src/infrasctructure/express.ts
+++ src/infrasctructure/express.ts
@@ -2,10 +2,12 @@ import express, { Express } from "express";
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "../modules/product-adm/repository/product.model";
 import { productRoute } from "./api/routes/product.route";
+import { clientRoute } from "./api/routes/client.route";
+import { ClientModel } from "../modules/client-adm/repository/client.model";
 
 export const app: Express = express();
 app.use(express.json());
-// app.use("/client", clientRoute);
+app.use("/client", clientRoute);
 app.use("/product", productRoute);
 // app.use("/invoice", invoiceRoute);
 // app.use("/checkout", checkoutRoute)
@@ -18,7 +20,7 @@ async function setupDb() {
     storage: ":memory:",
     logging: false,
   });
-  sequelize.addModels([ProductModel]);
+  sequelize.addModels([ProductModel, ClientModel]);
   await sequelize.sync();
 }
 setupDb();
